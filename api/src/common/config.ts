1c06dbc5062691c89c99a84b456b0c25159e96ed
root <root@ip-172-31-11-80.ap-south-1.compute.internal>
2026-06-13 11:06:28 +0000

    config.ts file modified for env

--- api/src/common/config.ts
+++ api/src/common/config.ts
@@ -1,9 +1,6 @@
 import * as dotenv from "dotenv";
-import path from "path";
 
-dotenv.config({
-  path: path.join(__dirname, "..", "..", ".env"),
-});
+dotenv.config();
 
 export const getConfig = (key: string) => {
   if (!key) {
